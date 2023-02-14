5c615feda9b47b228d2197b63e5f05b740cc90ad
j-dublon <j.johnson.dublon@gmail.com>
2023-02-14 11:40:46 +0000

    Add home page and background image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+import Head from "next/head";
+import Image from "next/image";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>J Dublon</title>
+      </Head>
+      <Image
+        src="/images/background.png"
+        alt="background trees"
+        fill
+        style={{ objectFit: "cover" }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
-import Head from "next/head";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import styles from "@/styles/home.module.css";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>J Dublon</title>
-      </Head>
-      <div>
-        <p>Home Screen</p>
-      </div>
+      <main className={styles.home}>
+        <h1 className={styles.home__title}>Hello, my name is Jodi Dublon.</h1>
+        <h1 className={styles.home__title}>I am a Software Engineer.</h1>
+        <Link href={"/portfolio"} className={styles.welcome__link}>
+          View my work <FontAwesomeIcon icon={faCircleArrowRight} />
+        </Link>
+      </main>
     </>
   );
 }
