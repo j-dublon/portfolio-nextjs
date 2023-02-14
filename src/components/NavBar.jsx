ded58a5de60958fff347a20a040e8b3d116470b7
j-dublon <j.johnson.dublon@gmail.com>
2023-02-14 12:02:40 +0000

    Add nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import styles from "@/styles/navbar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+export default function NavBar() {
+  return (
+    <div className={styles.navigation}>
+      <nav className={styles.leftNav}>
+        <a
+          href="https://User.com/j-dublon"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.leftNav__item}
+        >
+          <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jdublon/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.leftNav__item}
+        >
+          <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+        </a>
+      </nav>
+      <nav className="rightNav">
+        <Link href={"/"} className={styles.rightNav__item}>
+          Welcome
+        </Link>
+        <Link href={"/portfolio"} className={styles.rightNav__item}>
+          Portfolio
+        </Link>
+        <Link href={"/info"} className={styles.rightNav__item}>
+          About
+        </Link>
+        <Link href={"/contact"} className={styles.rightNav__item}>
+          Contact
+        </Link>
+      </nav>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import Head from "next/head";
 import Image from "next/image";
+import NavBar from "@/components/NavBar";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
         fill
         style={{ objectFit: "cover" }}
       />
+      <NavBar />
       <Component {...pageProps} />
     </>
   );
